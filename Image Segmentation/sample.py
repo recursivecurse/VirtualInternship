0cbe1238203b253f6a764f8f60b1c38b75828edd
recursivecurse <adityakumarm.cs18@rvce.edu.in>
2020-09-02 12:03:04 +0530

    Added Image Segmentation

---
+++ Image Segmentation/sample.py
@@ -0,0 +1,27 @@
+from skimage import io , img_as_float
+from matplotlib import pyplot as plt
+import numpy as np
+import cv2
+
+img = cv2.imread("d1.jpeg")
+
+img2 = img.reshape((-1,3))
+
+img2 = np.float32(img2)
+
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER , 10 , 1.0)
+
+k = 4
+attempts = 10
+
+ret ,label,center = cv2.kmeans(img2,k,None,criteria,attempts,cv2.KMEANS_RANDOM_CENTERS)
+
+center = np.uint8(center)
+
+res = center[label.flatten()] 
+res2 = res.reshape((img.shape))
+
+cv2.imwrite('segmented.jpeg',res2)   
+
+
+
